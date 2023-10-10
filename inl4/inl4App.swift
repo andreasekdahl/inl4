182c4a8d4183f40154b77dd58f7c135a71c72082
Andreas Ekdahl <fixed-term.andreas.ekdahl@se.bosch.com>
2023-10-10 22:08:52 +0200

    Initial Commit

---
+++ inl4/inl4App.swift
@@ -0,0 +1,17 @@
+//
+//  inl4App.swift
+//  inl4
+//
+//  Created by Andreas Ekdahl on 2023-10-10.
+//
+
+import SwiftUI
+
+@main
+struct inl4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
